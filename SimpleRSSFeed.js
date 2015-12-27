03765b634f9e11a6f603ab2aed790be4c399e04d
Brad Baago <brad.baago@linux.com>
2015-12-27 01:50:36 -0500

    Added link to post in title.

--- SimpleRSSFeed.js
+++ SimpleRSSFeed.js
@@ -25,7 +25,7 @@
             dataType: 'json',
             success: function (data) {
                 $.each(data.responseData.feed.entries, function() {
-                    $(container).append("<div class='"+settings.ItemClass+"'><div class='"+settings.ItemTitleClass+"'>"+$(this)[0].title+"</div><div class='"+settings.ItemContentClass+"'><p>"+$(this)[0].content+"</p></div></div>");
+                    $(container).append("<div class='"+settings.ItemClass+"'><div class='"+settings.ItemTitleClass+"'><a href='"+$(this)[0].link+"'>"+$(this)[0].title+"</a></div><div class='"+settings.ItemContentClass+"'><p>"+$(this)[0].content+"</p></div></div>");
                 });
                 if (typeof settings.callback == 'function') { // make sure the callback is a function
                     settings.callback.call(this); // brings the scope to the callback
