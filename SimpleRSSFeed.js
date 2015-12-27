16a39e6a1b2131d038906ceb23bc402099c5ee53
Brad Baago <brad.baago@linux.com>
2015-12-27 01:21:11 -0500

    Added google tracking and documentation.

--- SimpleRSSFeed.js
+++ SimpleRSSFeed.js
@@ -4,6 +4,7 @@
     $.fn.getFeed = function( options ) {
         var container = $(this);
         $(container).html("");
+        //Default values.
         var settings = $.extend({
             FeedURL: "notSet",
             ItemClass: "FeedItem",
@@ -11,6 +12,13 @@
             ItemContentClass: "ItemContent",
             callback: "notSet"
         }, options );
+        
+        //Returns error if no feed url is set.
+        if(settings.FeedURL == "notSet" || settings.FeedURL == "")
+        {
+            $(container).html("No Feed Url Set.");
+            return "Please set a FeedURL.";   
+        }
         
         $.ajax({
             url: document.location.protocol + '//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=10&callback=?&q=' + encodeURIComponent(settings.FeedURL),
