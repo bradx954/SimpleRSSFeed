00d4fef2e3b7376fbf747ac4551071e1168f1bb2
Brad Baago <brad.baago@linux.com>
2016-01-02 17:49:15 -0500

    Converted to Netbeans project.

---
+++ build/web/SimpleRSSFeed.js
@@ -0,0 +1,46 @@
+/* 
+Simple rss reader version 0.1 
+Author: Brad Baago
+more information at bradtech.ca
+no licensing of any kind use, distribute, and edit as you wish. 
+*/
+(function ( $ ) {
+ 
+    $.fn.getFeed = function( options ) {
+        var container = $(this);
+        $(container).html("");
+        //Default values.
+        var settings = $.extend({
+            FeedURL: "notSet",
+            ItemClass: "FeedItem",
+            ItemTitleClass: "ItemTitle",
+            ItemContentClass: "ItemContent",
+            callback: "notSet"
+        }, options );
+        
+        //Returns error if no feed url is set.
+        if(settings.FeedURL == "notSet" || settings.FeedURL == "")
+        {
+            $(container).html("No Feed Url Set.");
+            return "Please set a FeedURL.";   
+        }
+        
+        $.ajax({
+            url: document.location.protocol + '//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=10&callback=?&q=' + encodeURIComponent(settings.FeedURL),
+            dataType: 'json',
+            success: function (data) {
+                $.each(data.responseData.feed.entries, function() {
+                    $(container).append("<div class='"+settings.ItemClass+"'><div class='"+settings.ItemTitleClass+"'><a href='"+$(this)[0].link+"'>"+$(this)[0].title+"</a></div><div class='"+settings.ItemContentClass+"'><p>"+$(this)[0].content+"</p></div></div>");
+                });
+                if (typeof settings.callback == 'function') { // make sure the callback is a function
+                    settings.callback.call(this); // brings the scope to the callback
+                }
+            },
+            error: function(error)
+            {
+                alert(error);
+            }
+        });
+    };
+ 
+}( jQuery ));
